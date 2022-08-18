a3fe999079a493ee3d758dbf1c874829df4891f9
murigol <murilojrp@unochapeco.edu.br>
2022-08-18 13:32:17 -0300

    primeiro

---
+++ layouts/default.vue
@@ -0,0 +1,81 @@
+<template>
+  <v-app dark>
+    <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+      style="height:10%"
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-footer
+      :absolute="!fixed"
+      app
+      style="background-color:#073b5b; height: 10%"
+    >
+      <span
+      style="color:white"
+      >&copy; {{ `Produzido por @MurigolArts `}}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data () {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire'
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Universidades'
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,137 @@
+<template>
+<v-container
+  style="margin-top:5%"
+>
+<v-card
+  style="border:ridge"
+  outlined
+>
+    <v-card-title
+      style="color:#073b5b; margin-left:1%"
+    >
+      Universidades
+      <v-spacer></v-spacer>
+      <v-text-field
+        v-model="search"
+        append-icon="mdi-magnify"
+        label="Buscar"
+        single-line
+        hide-details
+      ></v-text-field>
+      <v-spacer></v-spacer>
+      <v-btn
+          color="#073b5b"
+          dark
+          class="mb-2"
+          v-bind="attrs"
+          v-on="on"
+          style="margin-right:1%;"
+        >
+          Nova Universidade
+        </v-btn>
+    </v-card-title>
+    <hr>
+    <v-data-table
+      :headers="headers"
+      :items="universities"
+      :search="search"
+    >
+    <template v-slot:item.actions="{ item }">
+      <v-icon
+        small
+        class="mr-2"
+        @click="editItem(item)"
+        style="color: #25abb3"
+      >
+        mdi-pencil
+      </v-icon>
+      <v-icon
+        small
+        @click="deleteItem(item)"
+        style="color: #cf5751"
+      >
+        mdi-delete
+      </v-icon>
+    </template>
+    </v-data-table>
+  </v-card>
+</v-container>
+</template>
+
+
+<script>
+export default {
+  name: 'IndexPage',
+  layout: 'default',
+    data () {
+      return {
+        search: '',
+        headers: [
+          {
+            text: 'Nome',
+            align: 'center',
+            sortable: true,
+            value: 'name',
+          },
+          { text: 'Dados', sortable: false, value: 'data' },
+          { text: 'Actions', value: 'actions'}
+        ],
+        universities: [
+          {
+            name: 'Universidade',
+            data: 'dados',
+          },
+          {
+            name: 'Universidade',
+            data: 'dados',
+          },
+          {
+            name: 'Universidade',
+            data: 'dados',
+          },
+          {
+            name: 'Universidade',
+            data: 'dados',
+          },
+          {
+            name: 'Universidade',
+            data: 'dados',
+          },
+          {
+            name: 'Universidade',
+            data: 'dados',
+          },
+          {
+            name: 'Universidade',
+            data: 'dados',
+          },
+          {
+            name: 'Universidade',
+            data: 'dados',
+          },
+          {
+            name: 'Universidade',
+            data: 'dados',
+          },
+          {
+            name: 'Universidade',
+            data: 'dados',
+          },
+        ],
+      }
+    },
+    methods: {
+      editItem (item) {
+        this.editedIndex = this.desserts.indexOf(item)
+        this.editedItem = Object.assign({}, item)
+        this.dialog = true
+      },
+
+      deleteItem (item) {
+        this.editedIndex = this.desserts.indexOf(item)
+        this.editedItem = Object.assign({}, item)
+        this.dialogDelete = true
+      },
+    }
+  }
+</script>
